88480d9307ffb5edae59bced83674b25ee970f0d
Nick Grismore <nicksk83@aol.com>
2017-10-14 17:27:19 -0400

    added ajax call for form

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-
 var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
 
 var PORT = process.env.PORT || 8080;
 
-var jsonParser = bodyParser.json()
  
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(urlencodedParser = bodyParser.urlencoded({ extended: true }))
 
 app.use(bodyParser.json({ type: 'application/*+json' }))
  
